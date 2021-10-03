8ace3a1f84995cc23d4fb2d8890e402805acb0a9
jason <2B3>
2021-10-03 12:23:20 -0400

    commit n89 ver 2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,18 @@
-import fs from "fs/promises"; // a standard node feature (fails on front-end)
-import path from "path";
-
-export async function getStaticProps(context) {
-  const projectRoot = process.cwd(); // "current" working directory
-  // equals the project's root directory, not the pages folder where we are
-
-  const filepath = path.join(projectRoot, "/public/data.json");
-  // builds an abosolute path to our data.json file
-
-  const jsonData = await fs.readFile(filepath);
-  const data = JSON.parse(jsonData);
-  // read the file with the absolute path specified as an arg, then parse
-
-  return { props: { info: data } };
-}
-
 export default function HomePage(props) {
-  console.log(props);
+  console.log(props)
   return (
     <ul>
-      {props.info.map((entry) => {
-        return <li key={entry.id}>{entry.name}</li>;
-      })}
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>{props.data[0].name}</li>
+      <li>Product 3</li>
     </ul>
   );
 }
+export async function getServerSideProps(context) {
+  // Place all backend code here, then return props based on what you fetch
+  const mockEndpoint= 'https://jsonplaceholder.typicode.com/users'  
+  const response = await fetch(mockEndpoint);
+  const data = await response.json();
+  return {props: {data: data}} // return props 
+} // search this URL to view whats inside this dummy hosted JSON file
