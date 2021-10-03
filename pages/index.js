8f3bfe3caff12ab9177ead392c56d8a4a0484c9b
jason <2B3>
2021-10-03 09:10:31 -0400

    getStaticProps requests work with Firebase backends

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react";
+
+export async function getStaticProps(context) {
+  const mockEndpoint= 'https://react-meals-7c9cb-default-rtdb.firebaseio.com/mealData.json'
+  const response = await fetch(mockEndpoint);
+  const data = await response.json();
+  return {props: {data: data}}
+}
+
 export default function HomePage(props) {
   console.log(props);
   return (
     <ul>
       <li>Product 1</li>
       <li>Product 2</li>
-      <li>{props.data[0].name}</li>
+      {/* <li>{props.data[0].name}</li> */}
       <li>Product 3</li>
     </ul>
   );
 }
-export async function getStaticProps(context) {
-  const mockEndpoint= 'https://jsonplaceholder.typicode.com/users'
-  const response = await fetch(mockEndpoint);
-  const data = await response.json();
-  return {props: {data: data}}
-}
+
 
