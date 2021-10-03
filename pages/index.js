fedc1c91a58b3e37b5176584ad9554adb8af3bad
jason <2B3>
2021-10-03 09:00:20 -0400

    commit n89 ver 1

--- pages/index.js
+++ pages/index.js
@@ -5,28 +5,15 @@ export default function HomePage(props) {
     <ul>
       <li>Product 1</li>
       <li>Product 2</li>
-      {/* <li>{props.jsonData.name}</li> */}
+      <li>{props.data[0].name}</li>
       <li>Product 3</li>
     </ul>
   );
 }
 export async function getStaticProps(context) {
-  const baseURL = "http://localhost:3000";
-  const response = await fetch(new URL("data.json", baseURL), {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  // const response = await fetch("http://localhost:3000/data.json");  // doesn't work either
-  const parsedInfo = await response.json(); // error here
-  return { props: { jsonData: parsedInfo } };
+  const mockEndpoint= 'https://jsonplaceholder.typicode.com/users'
+  const response = await fetch(mockEndpoint);
+  const data = await response.json();
+  return {props: {data: data}}
 }
-// INTENTION ▼ ▼
-// Use fetchAPI to grab data from our local data.json file
-// I'm aware I can simply import its content to the client side
-// I'm sticking to this approach for the sake of learning how getStaticProps works
 
-// ISSUES
-// The fetch URL must be absolute, and mine doesn't seem to be working
-// response.json() always crashes my site for some reason and I don't know why
